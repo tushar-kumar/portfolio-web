fe63fe644abc37a64573b12a3b2b786c1ca12036
Tushar Kumar <atul201kumar@gmail.com>
2022-08-19 03:04:03 +0530

    first

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,1 +1,18 @@
 import styled from "styled-components";
+
+export const Container = styled.footer`
+  background-color: #2b2b2b;
+  padding: 3rem 15rem;
+  margin-top: 15rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+
+  .logo{
+    font-size: 2.8rem;
+  }
+
+  
+
+`
