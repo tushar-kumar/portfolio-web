11a58c4fe321a72e51190fbd5922d681256262ab
Tushar Kumar <atul201kumar@gmail.com>
2022-08-19 03:04:43 +0530

    first

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -13,6 +13,26 @@ export const Container = styled.footer`
     font-size: 2.8rem;
   }
 
-  
+  p{
+    letter-spacing: 0.2rem;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    img{
+      width: 2.6rem;
+      animation: spinning 5s infinite linear;
+    }
+  }
+  .social-media{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 1rem;
+
+    img,span{
+      font-size: 3rem;
+      width: 3rem;
+    }
+  }
 
 `
